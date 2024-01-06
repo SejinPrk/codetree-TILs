a05c4d22f4a19168ca74a6e613b8d3018ba10d72
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-06 16:02:23 +0900

    Title: 시력 검사, Time: 167ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240106/시력 검사/eye-test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        if (1.0 <= a && 1.0 <= b) {
+            System.out.println("High");
+        }
+        else if(0.5 <= a && 0.5 <= b) {
+            System.out.println("Middle");
+        }else{
+            System.out.println("Low");
+        }
+    }
+}
