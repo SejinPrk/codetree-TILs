d5611f132da256cd05b45e6d0720b7b162c6072c
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-07 12:01:34 +0900

    Title: 코로나 메뉴얼, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240107/코로나 메뉴얼/covid-manual.java
+++ 240107/코로나 메뉴얼/covid-manual.java
@@ -1,33 +1,28 @@
 import java.util.Scanner;
 
-public class Main {
+    public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String sym = sc.next();
-        String a = "a";
-        int temp = sc.nextInt();
-        int count = 0;
+        char c1, c2, c3;
+        int t1, t2, t3;
 
-        for (int i = 0; i < 3; i++){
-         if (sym.equals("Y")) {
-            if(temp >= 37){
-                a = "A";
-                count += 1;
-            } else { 
-                a = "C";
-            }
-        } else {
-            if(temp >= 37){
-                a = "B";
-            } else{
-                a = "D"; 
-            }   
+        c1 = sc.next().charAt(0);t1 = sc.nextInt();
+        c2 = sc.next().charAt(0);
+        t2 = sc.nextInt();
+        c3 = sc.next().charAt(0);
+        t3 = sc.nextInt();
+
+        if(c1 == 'Y' && t1 >= 37) {
+            if((c2 == 'Y' && t2 >= 37) || (c3 == 'Y' && t3 >= 37))
+                System.out.println("E");
+            else
+                System.out.println("N");
         }
-        }
-        if(count >= 2){
-            System.out.println("E");
-        } else {
-            System.out.println("N");
+        else {
+            if((c2 == 'Y' && t2 >= 37) && (c3 == 'Y' && t3 >= 37))
+                System.out.println("E");
+            else
+                System.out.println("N");
         }
     }
 }
