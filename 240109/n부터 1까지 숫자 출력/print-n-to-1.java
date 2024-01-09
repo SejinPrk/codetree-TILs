baeefa411d4655d7df4bf3022b5cc8e3765c6b57
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-09 09:53:10 +0900

    Title: n부터 1까지 숫자 출력, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240109/n부터 1까지 숫자 출력/print-n-to-1.java
+++ 240109/n부터 1까지 숫자 출력/print-n-to-1.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();                
-        int i = 10;
 
-        while (i >= 1) {           
-            System.out.print(i+" ");
-            i--;                         
+        while (n >= 1) {           
+            System.out.print(n+" ");
+            n--;                         
         }
     }
 }
