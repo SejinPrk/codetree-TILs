be7a372ba3ea798e8c00ec21771159d367a93976
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-11 09:33:47 +0900

    Title: a 부터 b 까지, Time: 108ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240111/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a + " ");
+
+        for (int i = a; i <= b; i++) {
+            if (i % 2 != 0) {
+                i*=2;
+                System.out.print(i + " ");
+            }
+            else if(i%2 == 0) {
+                i+=3;
+                System.out.print(i +" ");
+            }
+        }
+    }
+}
