37b80db04abaa1c3e89669316fc2c28d0c4e0b03
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-16 09:38:59 +0900

    Title: 공약수의 유무, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240116/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 240116/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -9,7 +9,7 @@ public class Main {
         boolean satisfied = false;
         for (int i = a; i <= b; i++) {
 
-            if (1920 % i == 0 || 2880 % i == 0) {
+            if (1920 % i == 0 && 2880 % i == 0) {
                 satisfied = true;
             }
         }
