1077a66adb70ec1a324c0d694f8ba7994bbe8a03
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-17 12:12:53 +0900

    Title: 모두 3의 배수, Time: 244ms, Memory: 10MB, Status: Passed - CodeTree

--- 240117/모두 3의 배수/all-multiples-of-3.java
+++ 240117/모두 3의 배수/all-multiples-of-3.java
@@ -7,15 +7,15 @@ public class Main {
         boolean satisfied = true;
         for (int i = 1; i <= 5; i++) {
             int n = sc.nextInt();
-            if (n % 3 == 0) {
+            if (n % 3 != 0) {
                 satisfied = false;
             }
         }
         if (satisfied == true) {
-            System.out.println("0");
+            System.out.println("1");
         }
         else {
-            System.out.println("1");
+            System.out.println("0");
         }
     }
 }
