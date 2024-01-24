70565cf55a046b642328721c1c1f0c5d74822a8b
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-24 14:32:44 +0900

    Title: 알파벳 출력하기 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240124/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char x = 'A';
+
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < i+1; j++){
+                System.out.print(" ");
+            }
+            for(int j =0; j<n-i; j++){
+                if(x == 'Z'){
+                System.out.print((char)x++ + " ");
+                x = 'A';
+                } else {
+                    System.out.print((char)x++ + " ");
+                    x++;
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
