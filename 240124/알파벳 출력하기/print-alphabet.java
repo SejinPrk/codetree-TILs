e59124d8f000760a5b2fdb59af7ab855909e955a
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-24 14:26:47 +0900

    Title: 알파벳 출력하기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240124/알파벳 출력하기/print-alphabet.java
+++ 240124/알파벳 출력하기/print-alphabet.java
@@ -8,11 +8,11 @@ public class Main {
 
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
-                System.out.print(x);
-                x++;
                 if(x == 'Z'){
                 x = 'A';
-            }
+                }
+                System.out.print(x);
+                x++;
             }
             System.out.println();
         }
