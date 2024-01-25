e035aa3c1eb31adc8608930f5fcdfc33187671a4
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-25 10:02:55 +0900

    Title: 숫자 피라미드 2, Time: 295ms, Memory: 11MB, Status: Passed - CodeTree

--- 240125/숫자 피라미드 2/number-pyramid-2.java
+++ 240125/숫자 피라미드 2/number-pyramid-2.java
@@ -7,7 +7,7 @@ public class Main {
         int cnt = 1;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= i ; j++) {
+            for (int j = 1; j <= i ; j++) {
                 System.out.print(cnt + " ");
                 cnt++;
             }
