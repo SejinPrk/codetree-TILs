3a8900e61305c9598c1efa3ab7964e324760cf2a
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-26 15:35:10 +0900

    Title: 평균점수 구하기, Time: 140ms, Memory: 11MB, Status: Passed - CodeTree

--- 240126/평균점수 구하기/print-average.java
+++ 240126/평균점수 구하기/print-average.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double score[] = new double[100];
+        double score[] = new double[8];
         double sum = 0;
         double avg = 0;
 
         for (int i = 0; i < 8; i++) {
             score[i] = sc.nextDouble();
-            sum += score;
+            sum += score[i];
         }
         avg = sum / 8;
         System.out.printf("%.1f\n", avg);
