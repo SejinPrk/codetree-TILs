592939b3b7eb278189842f31a51bacc75cdf2c19
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-27 19:11:06 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240127/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int k = 0;
+
+        for(int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] == 0){
+                k = i;
+                break;
+            }
+        }
+        System.out.print(arr[k - 3] + arr[k - 2] + arr[k - 1]);
+    }
+}
