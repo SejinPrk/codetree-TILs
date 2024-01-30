236d5f2f0689a8ae1bace21b6051317b91e5cebf
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-30 17:11:04 +0900

    Title: 100 도달하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240130/100 도달하기/reach-100.java
+++ 240130/100 도달하기/reach-100.java
@@ -1,22 +1,22 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
-        
-        arr[1] = sc.nextInt();
-        arr[0] = 1;
-       
-        for (int i = 2; i < 10; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
-        }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] arr = new int[100];
 
-        for(int i = 0; i < 10; ++i){    
-            System.out.print(arr[i] + " ");
-            if (arr[i] > 100) {
-                break;
-            } 
-        }
+    arr[1] = sc.nextInt();
+    arr[0] = 1;
+
+    for (int i = 2; i < 10; i++) {
+      arr[i] = arr[i-1] + arr[i-2];
     }
+
+    for(int i = 0; i < 10; i++){
+      System.out.print(arr[i] + " ");
+      if (arr[i] > 100) {
+        break;
+      }
+    }
+  }
 }
