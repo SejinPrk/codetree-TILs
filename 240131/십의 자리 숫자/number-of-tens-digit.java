4135cd95f6518c87cb0ec79dcb535e0adbe62abc
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-01-31 09:36:51 +0900

    Title: 십의 자리 숫자, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240131/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int[] countArr = new int[10];
+
+        for (int i = 0; i < 99; i++) {
+            arr[i] = sc.nextInt();
+            countArr[arr[i]/10]++;
+            if(arr[i]==0){
+                break;
+            }
+        }
+
+         for (int i = 1; i <= 9; i++) {
+            System.out.println(i + " - " + countArr[i]);
+        }
+    }
+}
