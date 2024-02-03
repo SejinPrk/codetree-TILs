309cb14eb14d9368785a24abad6a7a4457b32d42
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-02-03 22:36:59 +0900

    Title: n개의 숫자 중 최대 2개, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240203/n개의 숫자 중 최대 2개/two-max-of-n-num.java
+++ 240203/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -10,8 +10,8 @@ public class Main {
         for (int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        for(int i=0; i<arr.length-1; i++){
-            for(int j=i+1; j<arr.length; j++){
+        for(int i=0; i < n-1; i++){
+            for(int j=i+1; j < n; j++){
                 if(arr[i]<arr[j]){ 
                     int tmp=arr[i];
                     arr[i]=arr[j];
@@ -20,6 +20,6 @@ public class Main {
             }
         }
 
-         System.out.print(arr[0]+" "+arr[1]);
+         System.out.printf("%d %d",arr[0], arr[1]);
     }
 }
