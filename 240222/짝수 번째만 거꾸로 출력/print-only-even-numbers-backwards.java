f86f6cc3d0545ebbbd2e6f2a4599c8b15a92ba07
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-02-22 16:54:57 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240222/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
+++ 240222/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
@@ -1,25 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    static double sum = 0;
-    static int cnt = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-        //StringTokenizer st = new StringTokenizer(br.readLine());
-        Stack<Character> st = new Stack<>();
-        String str = br.readLine();
-        for (int i = 0; i < str.length(); i++) {
-            if ((i + 1) % 2 == 0) {
-                st.push(str.charAt(i));
-            }
-        }
-        while (!st.empty()) {
-            System.out.print(st.pop());
+        int idx = str.length() - 1;
+        if(idx % 2 == 0) idx--;
+
+        for(int i = idx; i >= 0; i -= 2) {
+            System.out.print(str.charAt(i));
         }
     }
 }
