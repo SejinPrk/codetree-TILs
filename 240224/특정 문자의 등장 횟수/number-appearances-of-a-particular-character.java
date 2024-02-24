813b358fb8b631c467091ab0254b42d91967533b
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-02-24 17:35:25 +0900

    Title: 특정 문자의 등장 횟수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240224/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
+++ 240224/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
+        String str = sc.next();
         int cnt1 = 0; 
         int cnt2 = 0;
 
