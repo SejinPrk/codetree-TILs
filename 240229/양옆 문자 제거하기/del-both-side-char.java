48eb2ad60d305507503159720e096068cde5ea3d
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-02-29 17:45:54 +0900

    Title: 양옆 문자 제거하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240229/양옆 문자 제거하기/del-both-side-char.java
+++ 240229/양옆 문자 제거하기/del-both-side-char.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         int len = s.length();
-        s = s.substring(0, 2) + s.substring(3, len-2) + s.substring(len-1);
+        s = s.substring(0,1) + s.substring(2, len-2) + s.substring(len-1);
 
         System.out.println(s);
     }
