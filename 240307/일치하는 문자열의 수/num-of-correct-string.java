80611bef8c8865161c256c3166f6b278eb8c8098
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-03-07 09:44:50 +0900

    Title: 일치하는 문자열의 수, Time: 170ms, Memory: 10MB, Status: Passed - CodeTree

--- 240307/일치하는 문자열의 수/num-of-correct-string.java
+++ 240307/일치하는 문자열의 수/num-of-correct-string.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String A = sc.nextLine();
+        String A = sc.next();
         int cnt = 0; 
 
         for (int i = 0; i < n; i++) {
-            String str = sc.nextLine();
+            String str = sc.next();
             if (A.equals(str)) {
                 cnt++;
             }
