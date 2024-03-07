0fa68851c3a7543e26b9c16251e658241d82ed06
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-03-07 09:43:43 +0900

    Title: 일치하는 문자열의 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240307/일치하는 문자열의 수/num-of-correct-string.java
+++ 240307/일치하는 문자열의 수/num-of-correct-string.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String A = sc.nextLine();
-
+        sc.nextLine();
         int cnt = 0; 
 
         for (int i = 0; i < n; i++) {
