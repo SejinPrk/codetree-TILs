da314eaf8e772f7885cbce6f6c6190244cb64a89
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-07 10:48:41 +0900

    Title: 길이 단위 변환하기, Time: 166ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240607/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,8 @@
+let ft = 30.48;
+let mi = 160934;
+
+let ans_ft = 9.2 * ft;
+let ans_mi = 1.3 * mi;
+
+console.log("9.2ft = " + ans_ft.toFixed(1) + "cm");
+console.log("1.3mi = " + ans_mi.toFixed(1) + "cm");
