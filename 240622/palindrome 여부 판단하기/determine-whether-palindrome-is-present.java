5350e87845ea36285ecd1d26bea2948d01ba715a
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-22 17:32:31 +0900

    Title: palindrome 여부 판단하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240622/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    // 문자열 A가 Palindrom인지를 판단하는 함수
+    public static boolean isPalindrome(String A) {
+        int left = 0;
+        int right = A.length() - 1;
+        
+        // 문자열의 처음과 끝을 비교
+        while (left < right) {
+            if (A.charAt(left) != A.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine();
+        
+        // 회문인지 여부 검사 후 결과 출력
+        if (isPalindrome(A)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
