8982cc648b6aabdfa9501301730726163efe44a4
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-22 17:54:30 +0900

    Title: 두 정수에 대한 연산값, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240622/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    
+        compareNo(a, b);
+    }
+
+    public static void compareNo(int a, int b) {
+        if (a > b) {
+            a += 25;
+            b *= 2;
+        } else {
+            a *= 2;
+            b += 25;
+        }
+        System.out.println(a + " " + b);
+    }
+}
