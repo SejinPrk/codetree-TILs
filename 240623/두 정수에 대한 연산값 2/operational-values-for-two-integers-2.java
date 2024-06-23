5fd479c7f3e00dadb9070c0cfa10713eb809e9ef
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-23 13:20:50 +0900

    Title: 두 정수에 대한 연산값 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240623/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
+++ 240623/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -1,22 +1,43 @@
 import java.util.Scanner;
 
-public class Main {
+class IntWrapper {
+    int value;
+
+    public IntWrapper(int value) {
+        this.value = value;
+    }
+}
+
+public class Main {    
+    // 문제에서 설명한 대로 a와 b를 변경합니다.
+    // call by reference를 구현합니다.
+    public static void changeNumber(IntWrapper a, IntWrapper b) {
+        if(a.value > b.value) {
+            a.value *= 2;
+            b.value += 10;
+        }
+        else {
+            b.value *= 2;
+            a.value += 10;
+        }
+    
+        return;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        isMagicNumber(a, b);
-    }
+        IntWrapper aWrapper = new IntWrapper(a);
+        IntWrapper bWrapper = new IntWrapper(b);
 
-    public static void isMagicNumber(int a, int b) {
-        if ( a > b ) {
-            b += 10;
-            a *= 2;
-        } else {
-            a += 10;
-            b *= 2;
-        }
-        System.out.println (a + " " + b);
+        changeNumber(aWrapper, bWrapper);
+
+        a = aWrapper.value;
+        b = bWrapper.value;
+
+        System.out.print(a + " " + b);
     }
 }
