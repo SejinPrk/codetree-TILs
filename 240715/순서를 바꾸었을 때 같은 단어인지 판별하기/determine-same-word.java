c31631f41d56fb79f9db690060efe7a18063d0f2
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-07-15 10:07:47 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 280ms, Memory: 16MB, Status: Passed - CodeTree

--- 240715/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
+++ 240715/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
@@ -1,49 +1,23 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-    public static boolean areAnagrams(String word1, String word2) {
-        // 두 단어의 길이가 다르면 애너그램이 아님
-        if (word1.length() != word2.length()) {
-            return false;
-        }
-
-        // 두 단어를 소문자로 변환
-        word1 = word1.toLowerCase();
-        word2 = word2.toLowerCase();
-
-        // 각 단어의 문자 빈도를 저장할 배열
-        int[] charCount = new int[256];
-
-        // 첫 번째 단어의 각 문자의 빈도를 증가
-        for (char c : word1.toCharArray()) {
-            charCount[c]++;
-        }
-
-        // 두 번째 단어의 각 문자의 빈도를 감소
-        for (char c : word2.toCharArray()) {
-            charCount[c]--;
-        }
-
-        // 빈도 배열이 모두 0인지 확인
-        for (int count : charCount) {
-            if (count != 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
 
-        String str1 = sc.nextLine();
-        String str2 = sc.nextLine();
-        
-        if (areAnagrams(str1, str2)) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        char[] chars1 = str1.toCharArray();
+        Arrays.sort(chars1);
+        String sortedStr1 = new String(chars1);
+
+        char[] chars2 = str2.toCharArray();
+        Arrays.sort(chars2);
+        String sortedStr2 = new String(chars2); 
+
+        if(sortedStr1.equals(sortedStr2))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
     }
 }
