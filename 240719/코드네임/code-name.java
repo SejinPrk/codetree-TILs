55070afcf262ccaf90e859302e6d802b41de065b
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-07-19 23:43:57 +0900

    Title: 코드네임, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240719/코드네임/code-name.java
+++ 240719/코드네임/code-name.java
@@ -1,37 +1,46 @@
 import java.util.Scanner;
 
-class Student {
-    int kor, eng, math;
+class Agent {
+    private String codename;
+    private int score;
 
-    public Student() {
-        this.kor = 0;
-        this.eng = 0;
-        this.math = 0;
+    public Agent(String codename, int score) {
+        this.codename = codename;
+        this.score = score;
     }
 
-    public Student(int kor, int eng, int math){
-        this.kor = kor;
-        this.eng = eng;
-        this.math = math;
+    public String getCodename() {
+        return codename;
     }
-};
+
+    public int getScore() {
+        return score;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
+        Scanner scanner = new Scanner(System.in);
+        Agent[] agents = new Agent[5];
 
-        Student[] students = new Student[5];
-        for(int i = 0; i < 5; i++) {
-            int kor = sc.nextInt();
-            int eng = sc.nextInt();
-            int math = sc.nextInt();
-            students[i] = new Student(kor, eng, math);
+        // 5명의 요원 정보 입력받기
+        for (int i = 0; i < 5; i++) {
+            String codename = scanner.next();
+            int score = scanner.nextInt();
+            agents[i] = new Agent(codename, score);
         }
 
-        Student student3 = students[2];    // 세 번째 학생 정보
-        System.out.print("student3: ");
-        System.out.print(student3.kor + " ");
-        System.out.print(student3.eng + " ");
-        System.out.print(student3.math + " ");
+        // 점수가 가장 낮은 요원 찾기
+        Agent lowestScoreAgent = agents[0];
+        for (int i = 1; i < 5; i++) {
+            if (agents[i].getScore() < lowestScoreAgent.getScore()) {
+                lowestScoreAgent = agents[i];
+            }
+        }
+
+        // 결과 출력
+        System.out.println(lowestScoreAgent.getCodename() + " " + lowestScoreAgent.getScore());
+
+        scanner.close();
     }
 }
