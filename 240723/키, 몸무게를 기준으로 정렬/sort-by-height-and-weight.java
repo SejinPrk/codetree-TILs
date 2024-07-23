d55bfd1393d0877b59419225cdb38df5d1684e1c
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-07-23 14:05:17 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240723/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Person implements Comparable<Person> {
+    String name;
+    int height;
+    int weight;
+
+    Person (String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        if (this.height != other.height) {
+            return this.height - other.height;
+        }
+        return other.weight - this.weight;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine(); 
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = sc.nextLine().split(" ");
+
+            String name = input[0];
+            int height = Integer.parseInt(input[1]);
+            int weight = Integer.parseInt(input[2]);
+
+            people.add(new Person(name, height, weight));
+        }
+
+        Collections.sort(people);
+
+        for (Person person : people) {
+            System.out.println(person.name + " " + person.height + " " + person.weight);
+        }
+        sc.close();
+    }
+}
