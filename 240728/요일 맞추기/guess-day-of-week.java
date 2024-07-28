7658e8e122ba67afd35f27ff9dd4f9b6281d9537
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-07-28 13:59:49 +0900

    Title: 요일 맞추기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240728/요일 맞추기/guess-day-of-week.java
+++ 240728/요일 맞추기/guess-day-of-week.java
@@ -10,18 +10,26 @@ public class Main {
         int d2 = sc.nextInt();
 
         int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] dayNames = {"Sun", "Mon", "Tue", "Wed","Thu", "Fri", "Sat"};
+        String[] dayNames = {"Mon", "Tue", "Wed","Thu", "Fri", "Sat", "Sun"};
         
-        int totalDays = 0; 
+        int totalDays = 0;
 
-        for (int m = m1; m <= m2; m++) {
-            int start = (m == m1)? d1 : 1;
-            int end = (m == m2) ? d2 : daysInMonth[m];
-            totalDays += end - start + 1;
+        if (m1 == m2) {
+            totalDays = d2 - d1;
+        } else {
+            totalDays += daysInMonth[m1] - d1;
+            for (int m = m1 + 1; m < m2; m++) {
+                totalDays += daysInMonth[m];
+            }
+            totalDays += d2;
+        }
+
+        // 음수 값 처리
+        while (totalDays < 0) {
+            totalDays += 7;
         }
         
         int dayIndex = totalDays % 7;
-
         System.out.println(dayNames[dayIndex]);
 
         sc.close();
