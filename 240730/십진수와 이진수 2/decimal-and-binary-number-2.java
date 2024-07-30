596dc383339fe3090898e0fa76b4bfdaa5b0d376
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-07-30 21:01:45 +0900

    Title: 십진수와 이진수 2, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 240730/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_DIGIT = 20;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String binaryInput = sc.nextLine();
+        String binaryInput = sc.next();
 
         // 이진수 -> 십진수
-        int decimal = Integer.parseInt(binaryInput, 2);
+        int decimal = 0;
+        for (int i = 0; i < (int)binaryInput.length(); i++) {
+            decimal = decimal * 2 + (binaryInput.charAt(i) - '0');
+        }
 
         // *17
         decimal *= 17;
 
         // 다시 이진수로 변환
-        String result = Integer.toBinaryString(decimal);
-        System.out.println(result);
+        int[] digits = new int[MAX_DIGIT];
+        int cnt = 0;
 
+        while (true) {
+            if (decimal < 2) {
+                digits[cnt++] = decimal;
+                break;
+            }
+
+            digits[cnt++] = decimal % 2;
+            decimal /= 2;
+        }
+
+        // 이진수 출력
+        for (int i = cnt - 1; i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
         sc.close();
     }
 }
