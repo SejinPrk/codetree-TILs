3815d6d0f84f0339a372f32ca68bdcaf26481037
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-06 13:36:05 +0900

    Title: 계속 중첩되는 사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240806/계속 중첩되는 사각형/continuously-overlapping-squares.java
+++ 240806/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -2,31 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[][] rectangles = new int[n][4];
+        Scanner sc = new Scanner(System.in);
+        // n개의 직사각형 좌표를 입력받아 2차원 배열 rectangles에 저장
+        int n = sc.nextInt();
+        int[][] rect = new int[n][4];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 4; j++) {
-                rectangles[i][j] = scanner.nextInt();
+                rect[i][j] = sc.nextInt();
             }
         }
 
-        int result = calculateBlueArea(n, rectangles);
+        int result = calculatedBlueArea(n, rect);
         System.out.println(result);
 
-        scanner.close();
+        sc.close();
     }
 
-    public static int calculateBlueArea(int n, int[][] rectangles) {
+    // 파란색 영역의 넓이를 계산
+    public static int calculatedBlueArea(int n, int[][] rect) {
         int[][] grid = new int[201][201];
 
         for (int i = 0; i < n; i++) {
-            int color = i % 2 + 1; // 1: 빨간색, 2: 파란색
-            int x1 = rectangles[i][0] + 100;
-            int y1 = rectangles[i][1] + 100;
-            int x2 = rectangles[i][2] + 100;
-            int y2 = rectangles[i][3] + 100;
+            int color = i % 2 + 1; // 1: red, 2: blue
+            int x1 = rect[i][0] + 100;
+            int y2 = rect[i][1] + 100;
+            int x2 = rect[i][2] + 100;
+            int y2 = rect[i][3] + 100;
 
             for (int x = x1; x < x2; x++) {
                 for (int y = y1; y < y2; y++) {
