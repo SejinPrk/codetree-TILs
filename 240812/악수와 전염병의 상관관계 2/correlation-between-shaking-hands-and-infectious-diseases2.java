50fd9db4f08874076258f4ea65d51348b358616d
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-12 22:30:33 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 173ms, Memory: 12MB, Status: Passed - CodeTree

--- 240812/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 240812/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -1,57 +1,56 @@
 import java.util.*;
-
+class Developer {
+    public boolean infect;
+    public int num;
+    public int countInfections;
+    public Developer(int num, int k) {
+        infect = false;
+        this.num = num;
+        this.countInfections = k;
+    }
+    public void infection() {
+        this.infect = true;
+    }
+}
 public class Main {
+    public static int n;
+    public static int[] timeX = new int[251];
+    public static int[] timeY = new int[251];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 개발자 수
-        int K = sc.nextInt(); // 전염 가능 횟수
-        int P = sc.nextInt(); // 처음 감염된 개발자 번호
-        int T = sc.nextInt(); // 악수 횟수
-
-        boolean[] infected = new boolean[N + 1]; // 감염 여부
-        int[] infectionsLeft = new int[N + 1]; // 남은 전염 가능 횟수
-        
-        infected[P] = true;
-        infectionsLeft[P] = K;
-
-        // 악수 정보를 저장할 배열
-        int[][] handshakes = new int[T][3];
-        for (int i = 0; i < T; i++) {
-            handshakes[i][0] = sc.nextInt(); // 시간
-            handshakes[i][1] = sc.nextInt(); // x 개발자
-            handshakes[i][2] = sc.nextInt(); // y 개발자
+        n = sc.nextInt();
+        int k = sc.nextInt(), p = sc.nextInt(), t = sc.nextInt();
+        Developer[] d = new Developer[n];
+        for (int i = 0; i < n; i ++) {
+            d[i] = new Developer(i, k);
         }
-
-        // 시간 순으로 정렬
-        Arrays.sort(handshakes, (a, b) -> Integer.compare(a[0], b[0]));
-
-        // 각 악수 처리
-        for (int[] hs : handshakes) {
-            int x = hs[1], y = hs[2];
-            boolean xInfected = infected[x] && infectionsLeft[x] > 0;
-            boolean yInfected = infected[y] && infectionsLeft[y] > 0;
-            
-            if (xInfected) {
-                infected[y] = true;
-                infectionsLeft[x]--;
-            }
-            if (yInfected) {
-                infected[x] = true;
-                infectionsLeft[y]--;
-            }
-            
-            // 새로 감염된 경우 전염 가능 횟수 설정
-            if (xInfected && !yInfected) {
-                infectionsLeft[y] = K;
-            }
-            if (yInfected && !xInfected) {
-                infectionsLeft[x] = K;
+        d[p - 1].infection();
+        while (t -- > 0) {
+            int time = sc.nextInt();
+            int devX = sc.nextInt();
+            int devY = sc.nextInt();
+            timeX[time] = devX;
+            timeY[time] = devY;
+        }
+        for (int i = 0; i < timeX.length; i ++) {
+            if(timeX[i] != 0 && timeY[i] != 0) {
+                if(d[timeX[i] - 1].infect && d[timeX[i] - 1].countInfections > 0) {
+                    if(d[timeY[i] - 1].infect) d[timeY[i] - 1].countInfections --;
+                    d[timeY[i] - 1].infection();
+                    d[timeX[i] - 1].countInfections --;
+                    continue;
+                }
+                if(d[timeY[i] - 1].infect && d[timeY[i] - 1].countInfections > 0) {
+                    if(d[timeX[i] - 1].infect) d[timeX[i] - 1].countInfections --;
+                    d[timeX[i] - 1].infection();
+                    d[timeY[i] - 1].countInfections --;
+                    continue;
+                }
             }
         }
-
-        // 결과 출력
-        for (int i = 1; i <= N; i++) {
-            System.out.print(infected[i] ? "1" : "0");
+        for(int i = 0; i < n; i ++) {
+            if(d[i].infect) System.out.print(1);
+            else System.out.print(0);
         }
     }
 }
