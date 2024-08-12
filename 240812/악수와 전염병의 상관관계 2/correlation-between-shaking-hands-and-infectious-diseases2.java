c34bada163bc6c7b276259255837a89be960e271
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-12 22:27:51 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 311ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240812/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 240812/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -14,7 +14,7 @@ public class Main {
         infected[P] = true;
         infectionsLeft[P] = K;
 
-        // 악수 정보 저장
+        // 악수 정보를 저장할 배열
         int[][] handshakes = new int[T][3];
         for (int i = 0; i < T; i++) {
             handshakes[i][0] = sc.nextInt(); // 시간
@@ -22,25 +22,35 @@ public class Main {
             handshakes[i][2] = sc.nextInt(); // y 개발자
         }
 
-        // 시간 순 정렬
+        // 시간 순으로 정렬
         Arrays.sort(handshakes, (a, b) -> Integer.compare(a[0], b[0]));
 
         // 각 악수 처리
         for (int[] hs : handshakes) {
             int x = hs[1], y = hs[2];
-            if (infected[x] && infectionsLeft[x] > 0) {
+            boolean xInfected = infected[x] && infectionsLeft[x] > 0;
+            boolean yInfected = infected[y] && infectionsLeft[y] > 0;
+            
+            if (xInfected) {
                 infected[y] = true;
-                if (infectionsLeft[y] == 0) infectionsLeft[y] = K;
                 infectionsLeft[x]--;
             }
-            if (infected[y] && infectionsLeft[y] > 0) {
+            if (yInfected) {
                 infected[x] = true;
-                if (infectionsLeft[x] == 0) infectionsLeft[x] = K;
                 infectionsLeft[y]--;
+            }
+            
+            // 새로 감염된 경우 전염 가능 횟수 설정
+            if (xInfected && !yInfected) {
+                infectionsLeft[y] = K;
+            }
+            if (yInfected && !xInfected) {
+                infectionsLeft[x] = K;
             }
         }
 
-        for (int i = 0; i < N; i++) {
+        // 결과 출력
+        for (int i = 1; i <= N; i++) {
             System.out.print(infected[i] ? "1" : "0");
         }
     }
