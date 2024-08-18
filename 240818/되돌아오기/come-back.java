155fc3021ad1510c84531a0e34c3f73b1cb7cfdb
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-18 17:20:19 +0900

    Title: 되돌아오기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240818/되돌아오기/come-back.java
+++ 240818/되돌아오기/come-back.java
@@ -1,45 +1,67 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    static int N;
-    static int[] dx = {0, 1, 0, -1}; // N, E, S, W
-    static int[] dy = {1, 0, -1, 0}; // N, E, S, W
-    static int x = 0, y = 0;
-    static int time = 0;
-
-    static int getDirection(String dir) {
-        switch (dir) {
-            case "N": return 0;
-            case "E": return 1;
-            case "S": return 2;
-            case "W": return 3;
-            default: return -1;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String dirStr = st.nextToken();
-            int distance = Integer.parseInt(st.nextToken());
-            int dir = getDirection(dirStr);
-
-            for (int j = 0; j < distance; j++) {
-                x += dx[dir];
-                y += dy[dir];
-                time++;
-
-                if (x == 0 && y == 0) {
-                    System.out.println(time);
-                    return;
-                }
+    public static final int DIR_NUM = 4;    
+    public static int n, x, y;
+    public static int[] dx = new int[]{1, -1,  0, 0}; // E, W, S, N
+    public static int[] dy = new int[]{0,  0, -1, 1}; // E, W, S, N
+    public static int ans = -1;
+    public static int elapsedTime; // 지금까지 걸린 시간
+    
+    // dir 방향으로 dist 만큼 이동하는 함수
+    // 만약 시작지에 도달하면 true를 반환
+    public static boolean move(int dir, int dist) {
+        while(dist-- > 0) {
+            x += dx[dir];
+            y += dy[dir];
+            
+            // 이동시간 기록
+            elapsedTime++;
+    
+            // 시작지로 다시 돌아오면 답을 갱신
+            if(x == 0 && y == 0) {
+                ans = elapsedTime;
+                return true;
             }
         }
+        
+        return false;
+    }
 
-        System.out.println(-1);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        while(n-- > 0) {
+            char cDir = sc.next().charAt(0);
+            int dist = sc.nextInt();
+            
+            int dir;
+            switch(cDir) {
+                case 'E':
+                    dir = 0;
+                    break;
+                case 'W':
+                    dir = 1;
+                    break;
+                case 'S':
+                    dir = 2;
+                    break;
+                case 'N':
+                    dir = 3;
+                    break;
+                default:
+                    return -1;
+            }
+            
+            // 주어진 방향대로 dist 만큼 위치를 이동
+            boolean done = move(dir, dist);
+            
+            // 시작 위치에 도달 -> 종료
+            if(done)
+                break;
+        }
+        
+        System.out.print(ans);
     }
 }
