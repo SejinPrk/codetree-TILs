2004f1ed799e6dc8f3fedeedaf6cadbe1a9ee26a
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-20 14:43:02 +0900

    Title: 격자 위의 편안한 상태, Time: 517ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 240820/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int n;
+    private static boolean[][] grid;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+
+        grid = new boolean[n][n];
+
+        for (int i = 0; i < m; i++) {
+            int r = sc.nextInt() - 1;
+            int c = sc.nextInt() - 1;
+
+            grid[r][c] = true;
+            System.out.println(isComfortable(r, c) ? 1 : 0);
+        }
+
+        sc.close();
+    }
+
+    private static boolean isComfortable(int r, int c) {
+        int cnt = 0;
+    
+        if (r > 0 && grid[r-1][c]) cnt++;
+        if (r < n-1 && grid[r+1][c]) cnt++;
+        if (c > 0 && grid[r][c-1]) cnt++;
+        if (c < n-1 && grid[r][c+1]) cnt++;
+
+        return cnt == 3; // 편안한 상태 반환
+    }
+}
