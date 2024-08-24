20167387cbbdad9d7efc24cb7edc9ef00f477405
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-24 13:10:42 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 293ms, Memory: 16MB, Status: Passed - CodeTree

--- 240824/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
+++ 240824/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
@@ -4,17 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         int[][] matrix = new int[n][n];
-        
-        // 방향: 오른쪽, 위, 왼쪽, 아래
+
+        // 방향: 오른쪽(0), 위(1), 왼쪽(2), 아래(3)
         int[] dx = {0, -1, 0, 1};
         int[] dy = {1, 0, -1, 0};
 
         int x = n / 2; // 가운데 x 좌표
         int y = n / 2; // 가운데 y 좌표
-        int num = 1; // 시작 숫자
-        matrix[x][y] = num++; // 가운데 1 배치
-
+        int num = 1;   // 시작 숫자
+        matrix[x][y] = num++; // 가운데에 1 배치
+        
         int steps = 1; // 처음에는 1칸 이동
         while (num <= n * n) {
             // 방향 전환하며 이동
@@ -22,23 +23,27 @@ public class Main {
                 for (int step = 0; step < steps; step++) {
                     x += dx[i];
                     y += dy[i];
-
-                    if (x >= 0 && x < n && y >= 0 && y < n && matrix[x][y]==0) {
+                    
+                    if (x >= 0 && x < n && y >= 0 && y < n && matrix[x][y] == 0) {
                         matrix[x][y] = num++;
                     } else {
                         break;
                     }
+                    
                     if (num > n * n) break;
-                } 
+                }
                 if (i % 2 == 1) steps++; // 위쪽 또는 아래쪽으로 이동한 후에는 이동 칸수를 1 증가
             }
         }
+
+        // 결과 출력
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.println("%3d ", matrix[i][j]);
+                System.out.printf(matrix[i][j] + " ");
             }
             System.out.println();
         }
+
         sc.close();
     }
 }
