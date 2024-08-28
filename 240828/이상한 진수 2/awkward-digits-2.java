c7f2ce0d02954cad8d2531c11e8d568aca7bbdee
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-28 14:12:55 +0900

    Title: 이상한 진수 2, Time: 109ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240828/이상한 진수 2/awkward-digits-2.java
+++ 240828/이상한 진수 2/awkward-digits-2.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int findMaxValue(String a) {  // 주어진 이진수 문자열 a에서 가능한 최대값을 찾아내는 함수
-        // a가 1일 때 단 하나의 비트를 바꿔서 나오는 값은 0
+    // 주어진 이진수 문자열 a에서 가능한 최대값을 찾아내는 함수
+    public static int findMaxPossibleValue(String a) {
+        // a가 "1"일 때는 단 하나의 비트를 바꿔서 나오는 값은 "0" 뿐이다.
         if (a.equals("1")) {
             return 0;
         }
-
-                // a가 전부 1로 이루어져 있으면 가장 큰 수는 a의 첫 번째 비트를 0으로 바꾼 경우이다.
+        
+        // a가 전부 1로 이루어져 있으면 가장 큰 수는 a의 첫 번째 비트를 0으로 바꾼 경우이다.
         boolean allOnes = true;
         for (char c : a.toCharArray()) {
             if (c != '1') {
@@ -23,28 +24,34 @@ public class Main {
             modified[0] = '0';
             return Integer.parseInt(String.valueOf(modified), 2);
         }
-
-        int max = Integer.parseInt(a, 2); // 처음에 a 자체의 10진수 값 저장
-
-        // 각 자리에 대해 비트 하나씩 바꾸기
+        
+        int maxValue = Integer.parseInt(a, 2); // 처음에 a 자체의 10진수 값을 저장
+        
+        // 각 자리에 대해 하나씩 비트를 바꿔본다.
         for (int i = 0; i < a.length(); i++) {
-            // 현재 비트가 0 -> 1, 1 -> 0
+            // 현재 비트가 0이면 1로, 1이면 0으로 바꾼다.
             char[] modified = a.toCharArray();
             modified[i] = (modified[i] == '0') ? '1' : '0';
-
+            
             // 바꾼 이진수를 10진수로 변환
             int modifiedValue = Integer.parseInt(String.valueOf(modified), 2);
-
-            max = Math.max(max, modifiedValue);
+            
+            // 최대값 갱신
+            maxValue = Math.max(maxValue, modifiedValue);
         }
-        return max;
-    } 
+        
+        return maxValue;
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-
-        System.out.println(findMaxValue(a));
-        sc.close();
+        Scanner scanner = new Scanner(System.in);
+        
+        // 이진수 a 입력 받기
+        String a = scanner.nextLine();
+        
+        // 가능한 N 중 최댓값 출력
+        System.out.println(findMaxPossibleValue(a));
+        
+        scanner.close();
     }
 }
