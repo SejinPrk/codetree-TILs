0dd5bcbe67dfa536705aa15bea2235672ba02fe0
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-28 14:10:14 +0900

    Title: 이상한 진수 2, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240828/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int findMaxValue(String a) {
+        // 주어진 이진수 문자열 a에서 가능한 최대값을 찾아내는 함수
+        int max = Integer.parseInt(a, 2); // 처음에 a 자체의 10진수 값 저장
+
+        // 각 자리에 대해 비트 하나씩 바꾸기
+        for (int i = 0; i < a.length(); i++) {
+            // 현재 비트가 0 -> 1, 1 -> 0
+            char[] modified = a.toCharArray();
+            modified[i] = (modified[i] == '0') ? '1' : '0';
+
+            // 바꾼 이진수를 10진수로 변환
+            int modifiedValue = Integer.parseInt(String.valueOf(modified), 2);
+
+            max = Math.max(max, modifiedValue);
+        }
+        return max;
+    } 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        System.out.println(findMaxValue(a));
+        sc.close();
+    }
+}
