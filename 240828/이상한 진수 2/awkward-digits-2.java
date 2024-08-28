059075f52ba92b03baf54bbdc20286ac4055a698
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-28 14:19:46 +0900

    Title: 이상한 진수 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240828/이상한 진수 2/awkward-digits-2.java
+++ 240828/이상한 진수 2/awkward-digits-2.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static final int INT_MIN = Integer.MIN_VALUE;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String binary = sc.next();
         
-        // 각 자릿수를 바꿨을 때의 십진수 값 구하기
+        // 각 i번째 자릿수를 바꿨을 때의 십진수 값
         int ans = INT_MIN;
-        for(int i = 0; i < (int) binary.length(); i++) {
+        for(int i = 0; i < binary.length(); i++) {
             // i번째 자릿수 변경
-            binary = binary.substring(0, i) + (char)('0' + '1' - binary.charAt(i)) + binary.substring(i + 1);
-            // 십진수로 변환
-            int num = 0;
-            for(int j = 0; j < (int) binary.length(); j++)
-                num = num * 2 + (binary.charAt(j) - '0');
+            char[] chars = binary.toCharArray();
+            chars[i] = (chars[i] == '0') ? '1' : '0';
+            String modifiedBinary = new String(chars);
             
-            // 가능한 십진수 값 중 최댓값
+            // 바꾼 값을 십진수로 변환
+            int num = Integer.parseInt(modifiedBinary, 2);
+            
+            // 최댓값 갱신
             ans = Math.max(ans, num);
-            
-            // i번째 자릿수를 원래대로 돌려놓기
-            binary = binary.substring(0, i) + (char)('0' + '1' - binary.charAt(i)) + binary.substring(i + 1);
         }
         System.out.print(ans);
     }
