900730d2a4e50ffcaf53658505e29ff8b7407fca
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-09-16 17:10:32 +0900

    Title: 숫자 카운트, Time: 233ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 240916/숫자 카운트/numeric-count.java
+++ 240916/숫자 카운트/numeric-count.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+
         int[][] queries = new int[N][3];
-        
         for (int i = 0; i < N; i++) {
             queries[i][0] = sc.nextInt(); // 세 자리 수
             queries[i][1] = sc.nextInt(); // 1번 카운트
@@ -13,20 +13,20 @@ public class Main {
         }
 
         int possibleCnt = 0;
-        for (int i = 102; i <= 987; i++) {
-            if (isValidNumber(i) && checkAllQueries(i, queries)) {
-                possibleCnt++;
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 0; k <= 9; k++) {
+                    if (i != j && i != k && j != k) {
+                        int num = i * 100 + j * 10 + k;
+                        if (checkAllQueries(num, queries)) {
+                            possibleCnt++;
+                        }
+                    }
+                }
             }
         }
 
         System.out.println(possibleCnt);
-    }
-
-    private static boolean isValidNumber(int num) {
-        int hundreds = num / 100;
-        int tens = (num / 10) % 10;
-        int units = num % 10;
-        return hundreds != tens && hundreds != units && tens != units && hundreds != 0;
     }
 
     private static boolean checkAllQueries(int num, int[][] queries) {
@@ -41,13 +41,13 @@ public class Main {
 
     private static int[] getCounts(int num, int guess) {
         int[] counts = new int[2];
-        String numStr = String.valueOf(num);
-        String guessStr = String.valueOf(guess);
+        String numStr = String.format("%03d", num);  
+        String guessStr = String.format("%03d", guess);
         
         for (int i = 0; i < 3; i++) {
             if (numStr.charAt(i) == guessStr.charAt(i)) {
                 counts[0]++;
-            } else if (numStr.contains(String.valueOf(guessStr.charAt(i)))) {
+            } else if (numStr.indexOf(guessStr.charAt(i)) != -1) {
                 counts[1]++;
             }
         }
