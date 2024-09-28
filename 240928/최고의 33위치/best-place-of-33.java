2e342f1b236e0417a6bd2986682aa909cb5123d7
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-09-28 19:40:45 +0900

    Title: 최고의 33위치, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240928/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] grid = new int[N][N];
+
+        // 격자 정보 입력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        // 최대 동전 수를 저장할 변수
+        int maxCoins = 0;
+
+        // 3x3 격자가 N x N 격자를 벗어나지 않도록 검사
+        for (int i = 0; i <= N - 3; i++) {
+            for (int j = 0; j <= N - 3; j++) {
+                int currentCoins = 0;
+                
+                // 3x3 격자 내의 동전 수 세기
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        currentCoins += grid[i + x][j + y];
+                    }
+                }
+                
+                // 최대 동전 수 갱신
+                maxCoins = Math.max(maxCoins, currentCoins);
+            }
+        }
+        System.out.println(maxCoins);
+    }
+}
