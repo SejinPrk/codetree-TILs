42ace5bdc309ed5d64b0ce53061240921d3044cf
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-09-28 19:48:46 +0900

    Title: 행복한 수열의 개수, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240928/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 격자 크기 
+        int M = sc.nextInt(); // 연속된 숫자 수
+        sc.nextLine();
+
+        int[][] grid = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        int happySeqCnt = 0;
+
+        // 각 행 검사
+        for (int i = 0; i < N; i++) {
+            if (isHappySeq(grid[i], M)) {
+                happySeqCnt++;
+            }
+        }
+
+        // 각 열 검사
+        for (int j = 0; j < N; j++) {
+            int[] column = new int[N];
+            for (int i = 0; i < N; i++) {
+                column[i] = grid[i][j];
+            }
+            if (isHappySeq(column, M)) {
+                happySeqCnt++;
+            }
+        }
+        System.out.println(happySeqCnt);
+    }
+
+    private static boolean isHappySeq(int[] seq, int m) {
+        int cnt = 1;
+
+        for (int i = 1; i < seq.length; i++) {
+            if (seq[i] == seq[i-1]) {
+                cnt++;
+                // 연속된 숫자가 m개 이상일 때 행복한 수열
+                if (cnt >= m) {
+                    return true;
+                }
+            } else {
+                cnt = 1;
+            }
+        }
+        return false;
+    }
+}
